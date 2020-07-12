b42bec40fbab5d04a346a1f738e28d3ad8fc9f6f
Craig Barry <craigfbarry@gmail.com>
2020-07-12 18:05:32 +0930

    Added images and begun portfolio page.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,11 +8,11 @@ function Home() {
         <div className="container" id="content">
           <div id="hero-div1">
           <img src="https://cdn.pixabay.com/photo/2020/04/08/16/32/keyboard-5017973_960_720.jpg" className="img-fluid w-100" alt="laptop"/>
-          <h2 id="hero-text1">Intuitive design</h2>
+          <h3 id="hero-text1">Intuitive design</h3>
           <img src="assets/hero2.jpg" className="img-fluid w-100" alt="mobile"/>
-          <h2 id="hero-text2">Responsive websites</h2>
+          <h3 id="hero-text2">Responsive websites</h3>
           <img src="https://images.pexels.com/photos/943096/pexels-photo-943096.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" className="img-fluid w-100" alt="computer"/>
-          <h2 id="hero-text3">Full stack solutions</h2>
+          <h3 id="hero-text3">Full stack solutions</h3>
           </div>
       </div>
     </div>
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,17 +1,41 @@
 import React from "react";
 
+
+const projectDetails = [
+  {imageSource:"assets/wuber.png",imageAlt:"Wuber",description:"Wuber database Project",
+  githubURL:"https://github.com/craigfbarry/WUber",
+  projectURL:"https://wuber-wines.herokuapp.com/"},
+  {imageSource:"assets/nasa-slack.png",imageAlt:"NASA-slack",description:"NASA/Slack API Project",
+  githubURL:"https://github.com/craigfbarry/patient0",
+  projectURL:"https://lnxsa.com"},            
+  {imageSource:"assets/day-planner.png",imageAlt:"Day-planner",description:"Day Planner",
+  githubURL:"https://github.com/craigfbarry/day-planner",
+  projectURL:"https://craigfbarry.github.io/day-planner/"},
+  {imageSource:"assets/weather-dashboard.png",imageAlt:"Weather-Dashboard",description:"Weather API Dashboard",
+  githubURL:"https://github.com/craigfbarry/weather-dashboard",
+  projectURL:"https://craigfbarry.github.io/weather-dashboard"},
+  {imageSource:"assets/note-taker.png",imageAlt:"Note-Taker",description:"Note Taker",
+  githubURL:"https://github.com/craigfbarry/note-taker",
+  projectURL:"https://immense-springs-98803.herokuapp.com/"},
+  {imageSource:"assets/password-generator.png",imageAlt:"Password-Generator",description:"Password Generator",
+  githubURL:"https://github.com/craigfbarry/password-generator",
+  projectURL:"https://craigfbarry.github.io/password-generator/"}
+];
+
+
 function Portfolio() {
   return (
-    <div>
-      <h1>Portfolio</h1>
-      <h1>Portfolio2</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui
-        mauris, ut interdum nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus
-        porta. Nam quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam
-        semper imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed
-        rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+    <div className="container">
+      
+      <header className="row mt-5">
+          <h3 className="text-center mb-5">Portfolio</h3>
+      </header>
+      <div className="col-md-6 col-lg-4 px-0 px-md-4">
+          <img src="assets/password-generator.png" alt="Password-Generator"/>
+          <h3>Password Generator</h3>
+          <a href="https://github.com/craigfbarry/password-generator"></a>
+          <a href="https://craigfbarry.github.io/password-generator/"></a>
+      </div>
     </div>
   );
 }
