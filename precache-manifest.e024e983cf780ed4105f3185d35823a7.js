003d680c946bd16912d6bd9bebb8d6f14aee9181
Craig Barry <craigfbarry@gmail.com>
2020-07-15 17:47:05 +0930

    Updates

---
+++ precache-manifest.e024e983cf780ed4105f3185d35823a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ace4fb7657471e573e0c877e82c1d5a",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "c5a7f4cfd6d96543e5fa",
+    "url": "/react-portfolio/static/css/main.5c62b74a.chunk.css"
+  },
+  {
+    "revision": "2c8d8f94ed72b9bf06eb",
+    "url": "/react-portfolio/static/js/2.facc5857.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.facc5857.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5a7f4cfd6d96543e5fa",
+    "url": "/react-portfolio/static/js/main.159e0e28.chunk.js"
+  },
+  {
+    "revision": "a0bc6698c6899a75dd23",
+    "url": "/react-portfolio/static/js/runtime-main.90f4ca43.js"
+  }
+]);
